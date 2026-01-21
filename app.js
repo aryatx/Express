d1d431be2e4c4d2a77728ff5671a274573874859
ARY4T <aryathakurx@gmail.com>
2026-01-21 15:50:42 +0530

    20 Jan

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -17,7 +19,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'));
+    res.status(404).render('404', {pageTitle:"Page Not Found"});
 });
 
 app.listen(3000);
